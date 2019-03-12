6e99319575247cc0f922eabfbd707c4b8800f3f1
Ultraviolet <tierracook543@gmail.com>
2019-03-12 10:48:30 -0500

    seeds game and values

---
+++ models/seeds.sql
@@ -0,0 +1,134 @@
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Role Player", 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("7 Wonders", 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 3);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Cards Against Humanity", 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 4);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Bears vs Babies", 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 4);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Monopoly", 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 4);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Phase 10", 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Farkle",1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Chess", 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Dungeons and Dragons", 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 3);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Gloomhaven", 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 3);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("One Night Ultimate Warewolf", 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 3);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Captain Sonar", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Clue", 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Trivial Pursuit", 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 2);
+
+INSERT INTO games(game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Secret Hitler", 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 5);
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Connect 4", 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Settler of Catan",	0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Azul", 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Scrabble", 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Ticket to Ride", 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Carcassonne", 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Guess Who", 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Candy Land", 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Sorry", 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Battleship", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Life", 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Pictionary", 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Twister", 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Blokus", 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Ingenius", 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Apples to Apples",	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Operation", 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Dominoes",	0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("7 Wonders Duel", 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Scattergories",0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Crossword", 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Oregon Trail", 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Upwards",	0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Mancala", 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Skipbo", 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Tic-Tac-Toe", 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Video", 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Sudoku", 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Solitaire", 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1);
+
+INSERT INTO games (game_name, type_dice, type_card, type_board, type_other, genre_ActionAdventure, genre_Strategy, genre_LogicTrivia, genre_Mature, genre_Educational, genre_Fantasy, genre_PeoplePlacement, genre_RolePlayer, genre_Other, number_of_players)
+VALUES ("Sherades", 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 2);
+
